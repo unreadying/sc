e1164ed9a437aabe7b258b2a1e7883288307a181
unreadying <evan.f.stover@gmail.com>
2024-10-21 16:47:28 -0400

    selected some new photos and tweaked meta seo stuff

--- cars.js
+++ cars.js
@@ -28,9 +28,9 @@ function hover(event){
     }
     if(ccar<=totalcars && ccar>0){
         if(window.innerHeight<window.innerWidth){
-        document.getElementsByName(""+(ccar)+"")[0].src="fleet/"+(ccar+(1)/10)+".jpg";
+        document.getElementsByName(""+(ccar)+"")[0].src="fleet/"+(ccar+(1)/100)+".jpg";
         }else{
-            document.getElementsByName(""+(ccar)+"")[1].src="fleet/"+(ccar+(1)/10)+".jpg";
+            document.getElementsByName(""+(ccar)+"")[1].src="fleet/"+(ccar+(1)/100)+".jpg";
         }
     }
 }
@@ -73,9 +73,10 @@ function navigate(event){
 
 function imgsel(event){
         var selectedimg=event.target.id
-
+		
+		
         selectedimg = Number(selectedimg.replace(/\D/g, ""));
-
+		
         for(j=0;j<selectedimg;j++){
             nextimg();
         }
@@ -101,13 +102,14 @@ function menutog(){
 }
 
 function hide(event){
+	console.log(event.target.id);
     var img=document.getElementById(event.target.id);
 
     img.style.display="none";
 }
 
 function changefinder(){
-    for(i=0;i<8;i++){
+    for(i=0;i<20;i++){
         if(document.getElementById("gt"+(i+1)+"").style.display=="none"){
             maxchanges=i;
             return;
@@ -134,7 +136,10 @@ function nextimg(){
         if(c>maxchanges){
             c-=maxchanges+1;
         }
-        document.getElementById("gt"+(i)+"").src="fleet/"+(car+(Math.round(c*10)/100))+".jpg";
+		var val = c/100;
+		var carval = car+val;
+		carval=Math.round(carval*100)/100;
+        document.getElementById("gt"+(i)+"").src="fleet/"+carval+".jpg";
     }
 }
 
@@ -157,7 +162,10 @@ function previmg(){
         if(c>maxchanges){
             c-=maxchanges+1;
         }
-        document.getElementById("gt"+(i)+"").src="fleet/"+(car+(Math.round(c*10)/100))+".jpg";
+		var val = c/100;
+		var carval = car+val;
+		carval=Math.round(carval*100)/100;
+        document.getElementById("gt"+(i)+"").src="fleet/"+carval+".jpg";
     }
     
 }
@@ -186,17 +194,41 @@ function opengal(event){
     document.getElementById("gt6").style.display="inline";
     document.getElementById("gt7").style.display="inline";
     document.getElementById("gt8").style.display="inline";
+	document.getElementById("gt9").style.display="inline";
+	document.getElementById("gt10").style.display="inline";
+	document.getElementById("gt11").style.display="inline";
+	document.getElementById("gt12").style.display="inline";
+	document.getElementById("gt13").style.display="inline";
+	document.getElementById("gt14").style.display="inline";
+	document.getElementById("gt15").style.display="inline";
+	document.getElementById("gt16").style.display="inline";
+	document.getElementById("gt17").style.display="inline";
+	document.getElementById("gt18").style.display="inline";
+	document.getElementById("gt19").style.display="inline";
+	document.getElementById("gt20").style.display="inline";
     
     document.getElementById("gt0").src="fleet/"+car+".jpg";
-    document.getElementById("gt1").src="fleet/"+car+".1.jpg";
-    document.getElementById("gt2").src="fleet/"+car+".2.jpg";
-    document.getElementById("gt3").src="fleet/"+car+".3.jpg";
-    document.getElementById("gt4").src="fleet/"+car+".4.jpg";
-    document.getElementById("gt5").src="fleet/"+car+".5.jpg";
-    document.getElementById("gt6").src="fleet/"+car+".6.jpg";
-    document.getElementById("gt7").src="fleet/"+car+".7.jpg";
-    document.getElementById("gt8").src="fleet/"+car+".8.jpg";
-
+    document.getElementById("gt1").src="fleet/"+car+".01.jpg";
+    document.getElementById("gt2").src="fleet/"+car+".02.jpg";
+    document.getElementById("gt3").src="fleet/"+car+".03.jpg";
+    document.getElementById("gt4").src="fleet/"+car+".04.jpg";
+    document.getElementById("gt5").src="fleet/"+car+".05.jpg";
+    document.getElementById("gt6").src="fleet/"+car+".06.jpg";
+    document.getElementById("gt7").src="fleet/"+car+".07.jpg";
+    document.getElementById("gt8").src="fleet/"+car+".08.jpg";
+	document.getElementById("gt9").src="fleet/"+car+".09.jpg";
+	document.getElementById("gt10").src="fleet/"+car+".1.jpg";
+	document.getElementById("gt11").src="fleet/"+car+".11.jpg";
+	document.getElementById("gt12").src="fleet/"+car+".12.jpg";
+	document.getElementById("gt13").src="fleet/"+car+".13.jpg";
+	document.getElementById("gt14").src="fleet/"+car+".14.jpg";
+	document.getElementById("gt15").src="fleet/"+car+".15.jpg";
+	document.getElementById("gt16").src="fleet/"+car+".16.jpg";
+	document.getElementById("gt17").src="fleet/"+car+".17.jpg";
+	document.getElementById("gt18").src="fleet/"+car+".18.jpg";
+	document.getElementById("gt19").src="fleet/"+car+".19.jpg";
+	document.getElementById("gt20").src="fleet/"+car+".20.jpg";
+	
     document.getElementById("gallery").style.display="block";
     document.getElementById("screen").style.display="block";
 
